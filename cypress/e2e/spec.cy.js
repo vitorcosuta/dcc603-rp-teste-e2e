c2c66f0144632ba0970739ee74d32aab76b5400f
Vitor Costa <vitorcostamgbr@gmail.com>
2025-06-19 20:11:44 -0300

    Adicionando métodos de teste e2e com cypress
    Edição e conclusão de tarefas, e exclusão de tarefas concluídas

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -68,4 +68,89 @@ describe('TODOMvc App', () => {
       .children()
       .should('have.length', 2);
   });
+
+  it('Edita uma tarefa', () => {
+    cy.visit('');
+    cy.get('[data-cy=todo-input]')
+      .type('Jogar videogame{enter}');
+
+    cy.get('[data-cy=todos-list] li')
+      .should('be.visible')
+      .first()
+      .as('editedTodoItem');
+
+    cy.get('@editedTodoItem')
+      .dblclick();
+
+    cy.get('@editedTodoItem')
+        .find('.edit')
+        .should('be.visible')
+        .clear()
+        .type('Estudar para a prova{enter}');
+
+    cy.get('[data-cy=todos-list]')
+      .children('li')
+      .should('have.length', 1);
+
+    cy.get('[data-cy=todos-list] li')
+      .first()
+      .should('have.text', 'Estudar para a prova');
+  });
+
+  it('Completar e descompletar tarefas', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+      .type('Estudar para a prova{enter}')
+      .type('Lavar a louça{enter}')
+      .type('Lavar o cabelo{enter}');
+
+    cy.get('[data-cy=toggle-todo-checkbox]')
+      .each(($checkbox) => {
+        cy.wrap($checkbox).click({ force: true });
+      });
+
+    cy.get('[data-cy=todos-list] li')
+      .each(($li) => {
+        cy.wrap($li).should('have.class', 'completed');
+      });
+
+    cy.get('[data-cy=toggle-todo-checkbox]')
+      .each(($checkbox) => {
+        cy.wrap($checkbox).click({ force: true });
+      });
+
+    cy.get('[data-cy=todos-list] li')
+      .each(($li) => {
+        cy.wrap($li).should('not.have.class', 'completed');
+      });
+  });
+
+  it('Apagar tarefas completas', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+      .type('Estudar para a prova{enter}')
+      .type('Lavar a louça{enter}')
+      .type('Lavar o cabelo{enter}');
+
+    cy.get('[data-cy=todos-list] > li')
+      .eq(0)
+      .find('[data-cy=toggle-todo-checkbox]')
+      .click();
+
+    cy.get('[data-cy=todos-list] > li')
+      .eq(2)
+      .find('[data-cy=toggle-todo-checkbox]')
+      .click();
+
+    cy.get('button.clear-completed')
+      .click();
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'Lavar a louça');
+  });
 });
